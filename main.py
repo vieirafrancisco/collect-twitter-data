863c7f16d0714e89faf96684fd985a4abb07952d
Francisco <fvcneto.master98@gmail.com>
2019-03-17 11:02:01 -0300

    more arguments in commandline

--- main.py
+++ main.py
@@ -2,34 +2,7 @@ import os
 import sys
 import argparse
 
-import yaml
-import tweepy
-
-
-def get_twitter_api_instance():
-    with open("config.key.yaml", "r") as f:
-        credentials_file = yaml.load(f, Loader=yaml.Loader)
-
-    auth = tweepy.OAuthHandler(
-        credentials_file["CONSUMER_KEY"],
-        credentials_file["CONSUMER_SECRET"])
-
-    auth.set_access_token(
-        credentials_file["ACCESS_TOKEN"],
-        credentials_file["ACCESS_TOKEN_SECRET"]
-    )
-
-    api = tweepy.API(
-        auth,
-        wait_on_rate_limit=True,
-        wait_on_rate_limit_notify=True
-    )
-
-    if(not api):
-        print("Something wrong!")
-        return 0
-
-    return api
+import utils
 
 
 def create_configkey_file():
@@ -44,16 +17,38 @@ def get_commandline_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--in', dest='infile', nargs='?',
-        type=argparse.FileType('r'),
-        default=sys.stdin
+        type=str
     )
     parser.add_argument(
         '--cokey', dest='confkey', nargs='?',
-        type=bool,
+        type=string_to_bool_type,
         default=False
+    )
+    parser.add_argument(
+        '--user', nargs='?',
+        type=string_to_bool_type,
+        const=True, default=True
+    )
+    parser.add_argument(
+        '--tline', nargs='?',
+        type=string_to_bool_type,
+        const=True, default=True
+    )
+    parser.add_argument(
+        '--range', nargs=2,
+        type=int
     )
 
     return parser.parse_args()
+
+
+def string_to_bool_type(arg: str) -> bool:
+    if arg.lower() in ['true', 't', '1']:
+        return True
+    elif arg.lower() in ['false', 'f', '0']:
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Argument need to me boolean!")
 
 
 if __name__ == '__main__':
@@ -66,4 +61,5 @@ if __name__ == '__main__':
         else:
             print("already exist this file")
 
-    api = get_twitter_api_instance()
+    if not (args.tline or args.user):
+        raise Exception("tline and user argument can't be both False")
